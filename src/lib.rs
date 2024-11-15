4d19e763596d8afd2a30be7ecc07eb387c322a4a
Tenzenmaster <160365410+Tenzenmaster@users.noreply.github.com>
2024-11-14 22:58:58 -0500

    texturez

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,11 @@
-use std::sync::Arc;
+use std::{
+    f32::consts::FRAC_PI_2,
+    sync::Arc,
+};
 
 use bytemuck::{Pod, Zeroable};
+use image::GenericImageView as _;
+use nalgebra as na;
 use pollster::FutureExt as _;
 use wgpu::util::DeviceExt as _;
 use winit::{
@@ -61,7 +66,9 @@ struct State {
     queue: wgpu::Queue,
     pipeline: wgpu::RenderPipeline,
     vertex_buffer: wgpu::Buffer,
-    vertex_count: u32,
+    index_buffer: wgpu::Buffer,
+    index_count: u32,
+    texture_bind_group: wgpu::BindGroup,
 }
 
 impl State {
@@ -106,12 +113,27 @@ impl State {
             view_formats: vec![],
         };
 
+        let texture_bind_group_layout = device.create_bind_group_layout(&Texture::BIND_GROUP_LAYOUT_DESCRIPTOR);
+
+        let trollface = Texture::from_bytes(
+            &device,
+            &queue,
+            include_bytes!("Trollface.png"),
+            Some("Trollface"),
+        );
+        let trollface_bind_group = trollface.create_bind_group(&device, &texture_bind_group_layout);
+
         let vertex_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
             label: Some("Vertex Buffer"),
-            contents: bytemuck::cast_slice(&ColorVertex::TRIANGLE_VERTICES),
+            contents: bytemuck::cast_slice(&TextureVertex::RECT_VERTICES),
             usage: wgpu::BufferUsages::VERTEX,
         });
-        let vertex_count = ColorVertex::TRIANGLE_VERTICES.len() as u32;
+        let index_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Index Buffer"),
+            contents: bytemuck::cast_slice(&TextureVertex::RECT_INDICES),
+            usage: wgpu::BufferUsages::INDEX,
+        });
+        let index_count = 6;
 
         let shader_module = device.create_shader_module(wgpu::ShaderModuleDescriptor {
             label: None,
@@ -119,7 +141,7 @@ impl State {
         });
         let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
             label: None,
-            bind_group_layouts: &[],
+            bind_group_layouts: &[&texture_bind_group_layout],
             push_constant_ranges: &[],
         });
         let pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
@@ -129,7 +151,7 @@ impl State {
                 module: &shader_module,
                 entry_point: Some("vs_main"),
                 compilation_options: wgpu::PipelineCompilationOptions::default(),
-                buffers: &[ColorVertex::LAYOUT],
+                buffers: &[TextureVertex::LAYOUT],
             },
             fragment: Some(wgpu::FragmentState {
                 module: &shader_module,
@@ -168,7 +190,9 @@ impl State {
             queue,
             pipeline,
             vertex_buffer,
-            vertex_count,
+            index_buffer,
+            index_count,
+            texture_bind_group: trollface_bind_group,
         }
     }
 
@@ -198,8 +222,10 @@ impl State {
             timestamp_writes: None,
         });
         render_pass.set_pipeline(&self.pipeline);
+        render_pass.set_bind_group(0, &self.texture_bind_group, &[]);
         render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
-        render_pass.draw(0..self.vertex_count, 0..1);
+        render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
+        render_pass.draw_indexed(0..self.index_count, 0, 0..1);
 
         drop(render_pass);
         self.queue.submit(std::iter::once(encoder.finish()));
@@ -237,4 +263,185 @@ impl ColorVertex {
         Self { pos: [-0.5, -0.5, 0.0], color: [0.0, 1.0, 0.0] },
         Self { pos: [0.5, -0.5, 0.0], color: [0.0, 0.0, 1.0] },
     ];
+}
+
+#[repr(C)]
+#[derive(Clone, Copy, Pod, Zeroable)]
+struct TextureVertex {
+    pos: [f32; 3],
+    tex_coords: [f32; 2],
+}
+
+impl TextureVertex {
+    const LAYOUT: wgpu::VertexBufferLayout<'static> = wgpu::VertexBufferLayout {
+        array_stride: size_of::<Self>() as u64,
+        step_mode: wgpu::VertexStepMode::Vertex,
+        attributes: &[
+            wgpu::VertexAttribute {
+                format: wgpu::VertexFormat::Float32x3,
+                offset: 0,
+                shader_location: 0,
+            },
+            wgpu::VertexAttribute {
+                format: wgpu::VertexFormat::Float32x2,
+                offset: size_of::<[f32; 3]>() as u64,
+                shader_location: 1,
+            },
+        ],
+    };
+
+    const RECT_VERTICES: [Self; 4] = [
+        Self { pos: [0.5, 0.5, 0.0], tex_coords: [1.0, 0.0] },
+        Self { pos: [-0.5, 0.5, 0.0], tex_coords: [0.0, 0.0] },
+        Self { pos: [-0.5, -0.5, 0.0], tex_coords: [0.0, 1.0] },
+        Self { pos: [0.5, -0.5, 0.0], tex_coords: [1.0, 1.0] },
+    ];
+
+    const RECT_INDICES: [u16; 6] = [
+        0, 1, 2,
+        0, 2, 3,
+    ];
+}
+
+struct Texture {
+    texture: wgpu::Texture,
+    view: wgpu::TextureView,
+    sampler: wgpu::Sampler,
+}
+
+impl Texture {
+    fn from_bytes(
+        device: &wgpu::Device,
+        queue: &wgpu::Queue,
+        bytes: &[u8],
+        label: Option<&str>,
+    ) -> Self {
+        let image = image::load_from_memory(bytes)
+            .expect("Failed to load image from memory");
+        Self::from_image(device, queue, &image, label)
+    }
+
+    fn from_image(
+        device: &wgpu::Device,
+        queue: &wgpu::Queue,
+        image: &image::DynamicImage,
+        label: Option<&str>,
+    ) -> Self {
+        let rgba = image.to_rgba8();
+        let dimensions = image.dimensions();
+        let size = wgpu::Extent3d {
+            width: dimensions.0,
+            height: dimensions.1,
+            depth_or_array_layers: 1,
+        };
+
+        let texture = device.create_texture(&wgpu::TextureDescriptor {
+            label,
+            size,
+            format: wgpu::TextureFormat::Rgba8UnormSrgb,
+            dimension: wgpu::TextureDimension::D2,
+            mip_level_count: 1,
+            sample_count: 1,
+            usage: wgpu::TextureUsages::TEXTURE_BINDING | wgpu::TextureUsages::COPY_DST,
+            view_formats: &[],
+        });
+        queue.write_texture(
+            wgpu::ImageCopyTexture {
+                texture: &texture,
+                mip_level: 0,
+                origin: wgpu::Origin3d::ZERO,
+                aspect: wgpu::TextureAspect::All,
+            },
+            &rgba,
+            wgpu::ImageDataLayout {
+                offset: 0,
+                bytes_per_row: Some(size.width * 4),
+                rows_per_image: Some(size.height),
+            },
+            size,
+        );
+
+        let view = texture.create_view(&wgpu::TextureViewDescriptor::default());
+
+        let sampler = device.create_sampler(&wgpu::SamplerDescriptor {
+            label,
+            address_mode_u: wgpu::AddressMode::ClampToEdge,
+            address_mode_v: wgpu::AddressMode::ClampToEdge,
+            address_mode_w: wgpu::AddressMode::ClampToEdge,
+            mag_filter: wgpu::FilterMode::Linear,
+            min_filter: wgpu::FilterMode::Nearest,
+            mipmap_filter: wgpu::FilterMode::Nearest,
+            ..Default::default()
+        });
+
+        Self {
+            texture,
+            view,
+            sampler,
+        }
+    }
+
+    fn create_bind_group(&self, device: &wgpu::Device, layout: &wgpu::BindGroupLayout) -> wgpu::BindGroup {
+        device.create_bind_group(&wgpu::BindGroupDescriptor {
+            label: Some("Texture Bind Group"),
+            layout: &layout,
+            entries: &[
+                wgpu::BindGroupEntry {
+                    binding: 0,
+                    resource: wgpu::BindingResource::TextureView(&self.view),
+                },
+                wgpu::BindGroupEntry {
+                    binding: 1,
+                    resource: wgpu::BindingResource::Sampler(&self.sampler),
+                },
+            ],
+        })
+    }
+
+    const BIND_GROUP_LAYOUT_DESCRIPTOR: wgpu::BindGroupLayoutDescriptor<'static> = wgpu::BindGroupLayoutDescriptor {
+        label: Some("Texture Bind Group Layout"),
+        entries: &[
+            wgpu::BindGroupLayoutEntry {
+                binding: 0,
+                visibility: wgpu::ShaderStages::FRAGMENT,
+                count: None,
+                ty: wgpu::BindingType::Texture {
+                    sample_type: wgpu::TextureSampleType::Float { filterable: true },
+                    view_dimension: wgpu::TextureViewDimension::D2,
+                    multisampled: false,
+                }
+            },
+            wgpu::BindGroupLayoutEntry {
+                binding: 1,
+                visibility: wgpu::ShaderStages::FRAGMENT,
+                count: None,
+                ty: wgpu::BindingType::Sampler(wgpu::SamplerBindingType::Filtering),
+            },
+        ],
+    };
+}
+
+#[rustfmt::skip]
+const OPENGL_TO_WGPU_MATRIX: na::Matrix4<f32> = na::Matrix4::new(
+    1.0, 0.0, 0.0, 0.0,
+    0.0, 1.0, 0.0, 0.0,
+    0.0, 0.0, 0.5, 0.5,
+    0.0, 0.0, 0.0, 1.0,
+);
+
+const SAFE_FRAC_PI_2: f32 = FRAC_PI_2 - 0.0001;
+
+struct Camera(na::Isometry3<f32>);
+
+struct Projection {
+    aspect: f32,
+    fovy: f32,
+    z_near: f32,
+    z_far: f32,
+}
+
+impl Projection {
+    fn to_matrix(&self) -> na::Matrix4<f32> {
+        OPENGL_TO_WGPU_MATRIX * na::Matrix4::new_perspective(self.aspect, self.fovy, self.z_near, self.z_far)
+    }
 }
